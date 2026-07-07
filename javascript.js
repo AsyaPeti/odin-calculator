a4e9b7f8c9f69b9c880c0d38ef253dee53ebe995
AsyaPeti <62142349+AsyaPeti@users.noreply.github.com>
2026-07-07 19:37:44 +0300

    Create a variable including all the digit buttons
    This variable may eliminate the need for separate variables for each
    digit button.

--- javascript.js
+++ javascript.js
@@ -32,16 +32,17 @@ let num2;
 let op;
 
 // These are the variables for the digit buttons
-const zero = document.querySelector("#zero");
-const one = document.querySelector("#one");
-const two = document.querySelector("#two");
-const three = document.querySelector("#three");
-const four = document.querySelector("#four");
-const five = document.querySelector("#five");
-const six = document.querySelector("#six");
-const seven = document.querySelector("#seven");
-const eight = document.querySelector("#eight");
-const nine = document.querySelector("#nine");
+// const zero = document.querySelector("#zero");
+// const one = document.querySelector("#one");
+// const two = document.querySelector("#two");
+// const three = document.querySelector("#three");
+// const four = document.querySelector("#four");
+// const five = document.querySelector("#five");
+// const six = document.querySelector("#six");
+// const seven = document.querySelector("#seven");
+// const eight = document.querySelector("#eight");
+// const nine = document.querySelector("#nine");
+const digits = document.querySelectorAll(".digit");
 
 // This is the variable for the display
 const display = document.querySelector("#display");
