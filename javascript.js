348190caea53e1e25f80e39d074e389e351ea3e6
AsyaPeti <62142349+AsyaPeti@users.noreply.github.com>
2026-07-07 17:57:00 +0300

    Add a string to a number conversion
    The functions performed operations on variables that could be strings.
    These functions were updated to convert those variables to numbers.

--- javascript.js
+++ javascript.js
@@ -1,21 +1,21 @@
 // This function adds two numbers together
 function addition(num1, num2) {
-  return num1 + num2;
+  return +num1 + +num2;
 }
 
 // This function subtracts the second number from the first
 function subtraction(num1, num2) {
-  return num1 - num2;
+  return +num1 - +num2;
 }
 
 // This function multiplies two numbers
 function multiplication(num1, num2) {
-  return num1 * num2;
+  return +num1 * +num2;
 }
 
 // This function divides the first number by the second
 function division(num1, num2) {
-  return num1 / num2;
+  return +num1 / +num2;
 }
 
 // These are the variables and operators that will be used in the calculations
