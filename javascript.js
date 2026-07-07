3b53f7d529b124b1f488e108978760021cacc357
AsyaPeti <62142349+AsyaPeti@users.noreply.github.com>
2026-07-07 17:41:22 +0300

    Add comments explaining the functions

--- javascript.js
+++ javascript.js
@@ -1,15 +1,19 @@
+// This function adds two numbers together
 function addition(num1, num2) {
   return num1 + num2;
 }
 
+// This function subtracts the second number from the first
 function subtraction(num1, num2) {
   return num1 - num2;
 }
 
+// This function multiplies two numbers
 function multiplication(num1, num2) {
   return num1 * num2;
 }
 
+// This function divides the first number by the second
 function division(num1, num2) {
   return num1 / num2;
 }
