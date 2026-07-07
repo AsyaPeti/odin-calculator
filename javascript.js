76c44cda1d859060d91ad3933ba49f86162ce0b6
AsyaPeti <62142349+AsyaPeti@users.noreply.github.com>
2026-07-07 19:17:58 +0300

    Create variables for the digit buttons
    The variables for the digit buttons were created to track which buttons
    a user clicked and to update the variables for calculations
    correspondingly.

--- javascript.js
+++ javascript.js
@@ -30,3 +30,15 @@ function operate(num1, num2, op) {
 let num1;
 let num2;
 let op;
+
+// These are the variables for the digit buttons
+const zero = document.querySelector("#zero");
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
