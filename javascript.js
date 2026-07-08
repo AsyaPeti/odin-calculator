1c803fa2c0f51577de25f572632fe2fc32ff51a5
AsyaPeti <62142349+AsyaPeti@users.noreply.github.com>
2026-07-08 15:51:47 +0300

    Add code for the first number and the operator
    If the first number and the operator are present, then the digits for
    the second number or the operator can be entered. Also calculations can
    be performed or all values can be deleted.

--- javascript.js
+++ javascript.js
@@ -21,9 +21,9 @@ function division(num1, num2) {
 // This function calls the calculation function based on the given operator
 function operate(num1, num2, op) {
   if (op === "+") return addition(num1, num2);
-  if (op === "-") return subtraction(num1, num2);
-  if (op === "*") return multiplication(num1, num2);
-  if (op === "/") return division(num1, num2);
+  if (op === "−") return subtraction(num1, num2);
+  if (op === "×") return multiplication(num1, num2);
+  if (op === "÷") return division(num1, num2);
 }
 
 // These are the variables that will be used in the calculations
@@ -43,7 +43,7 @@ buttons.addEventListener("click", (event) => {
   let targetBtn = event.target;
 
   // The actions to take when there is no values of calculating variables
-  if (!(+num1 || op || num2)) {
+  if (!(num1 || op || num2)) {
     if (targetBtn.className === "digit") {
       num1 = targetBtn.textContent;
       display.textContent = targetBtn.textContent;
@@ -52,8 +52,13 @@ buttons.addEventListener("click", (event) => {
   // The actions to take when there is only num1 has a value
   } else if (num1 && !(op || num2)) {
     if (targetBtn.className === "digit") {
+      if (+num1) {
       num1 += targetBtn.textContent;
       display.textContent += targetBtn.textContent;
+      } else {
+        num1 = targetBtn.textContent;
+        display.textContent = targetBtn.textContent;
+      }
     }
 
     if (targetBtn.className === "operator") {
@@ -64,9 +69,32 @@ buttons.addEventListener("click", (event) => {
       num1 = null;
       display.textContent = "";
     }
-  }
 
   // The actions to take when there are num1 and op have values
+  } else if (num1 && op && !num2) {
+    if (targetBtn.className === "digit") {
+      num2 = targetBtn.textContent;
+      display.textContent = targetBtn.textContent;
+    }
+
+    if (targetBtn.className === "operator") {
+      op = targetBtn.textContent;
+    }
+
+    if (targetBtn.id === "calculate") {
+      num2 = num1;
+      console.log(num1, op, num2);
+      result = operate(num1, num2, op);
+      num1 = result;
+      display.textContent = result;
+    }
+
+    if (targetBtn.id === "all-clear") {
+      num1 = null;
+      op = null;
+      display.textContent = "";
+    }
+  }
 
   // The actions to take when all calculating variables have values
 
